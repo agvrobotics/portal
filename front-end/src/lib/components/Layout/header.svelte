4fd330c000005fb8148b8dba7b4318d8d39b7bd0
sierra-95 <michaelmachohi@gmail.com>
2025-05-17 00:16:04 +0300

    profile

--- front-end/src/lib/components/Layout/header.svelte
+++ front-end/src/lib/components/Layout/header.svelte
@@ -2,13 +2,13 @@
   import {Profile} from '$lib'
 </script>
 <header class="w-full flex justify-between h-[70px] border-b-1 border-gray-300">
-  <div class="flex">
+  <div class="flex items-center">
     <div class="menu-button">
       <div class="bar"></div>
       <div class="bar"></div>
       <div class="bar"></div>
     </div>
-    <img class="h-[50px]" src='/dekut.png' alt="logo"/>
+    <h2 class="text-3xl ml-3 max-[500px]:text-2xl">2025agvrobotics</h2>
   </div>
   <Profile/>
 </header>
