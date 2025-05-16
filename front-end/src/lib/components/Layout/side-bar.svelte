7d5363ba9ebf4a2b6dc7201b047ad68ee547b2bd
sierra-95 <michaelmachohi@gmail.com>
2025-05-16 19:36:40 +0300

    sidebar

--- front-end/src/lib/components/Layout/side-bar.svelte
+++ front-end/src/lib/components/Layout/side-bar.svelte
@@ -1,8 +1,8 @@
 <script>
     import { Home, Settings, MapIcon } from '@lucide/svelte';
 </script>
-<div id='sidebar' class="w-[100px] flex flex-col gap-3 items-center pt-5 border-r-1 border-gray-300">
-    <Home size={35}/>
-    <MapIcon size={35}/>
-    <Settings size={35}/>
+<div id='sidebar' class="w-[70px] flex flex-col gap-3 items-center pt-5 border-r-1 border-gray-300">
+    <Home size={50} absoluteStrokeWidth/>
+    <MapIcon size={50} absoluteStrokeWidth/>
+    <Settings size={50} absoluteStrokeWidth/>
 </div>
