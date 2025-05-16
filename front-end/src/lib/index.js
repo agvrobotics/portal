53fa0406889d5f41ae8acf8937b37a242dd0461e
sierra-95 <michaelmachohi@gmail.com>
2025-05-17 00:45:49 +0300

    profile

--- front-end/src/lib/index.js
+++ front-end/src/lib/index.js
@@ -4,3 +4,5 @@ export { default as Header } from './components/Layout/header.svelte';
 export { default as SideBar } from './components/Layout/side-bar.svelte';
 export { default as SideMenu } from './components/Layout/side-menu.svelte';
 export {default as Profile} from './components/Layout/profile.svelte';
+export {default as CircularProgress} from './components/CircularProgress.svelte'
+export {default as LinearProgress} from './components/LinearProgress.svelte'
