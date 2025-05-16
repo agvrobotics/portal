bc2248cb5a362eed594ca221b427330dc0ada38b
sierra-95 <michaelmachohi@gmail.com>
2025-05-16 20:42:56 +0300

    Sidebar

--- front-end/src/lib/components/Layout/side-bar.svelte
+++ front-end/src/lib/components/Layout/side-bar.svelte
@@ -23,10 +23,13 @@
 	}
 	.icon-active {
 		background-color: rgb(236, 244, 255);
+		border-top: 1px solid rgb(185, 214, 255);
+        border-bottom: 1px solid rgb(185, 214, 255);
 	}
 </style>
 
 <div id="sidebar" class="w-[70px] flex flex-col gap-3 items-center pt-5 border-r-1 border-gray-300">
+	<!--Dont change pt-5-->
 	<div class="icon-base {isActive(routes.home) ? 'icon-active' : ''}">
 		<Home size={50} absoluteStrokeWidth color="rgb(0, 43, 103)" />
 	</div>
--- front-end/src/lib/components/Layout/side-menu.svelte
+++ front-end/src/lib/components/Layout/side-menu.svelte
@@ -1,1 +1,55 @@
-<div class="swiper-slide menu border-r-1 border-gray-300">Menu slide</div>
+<script>
+	import { Home, Settings, MapIcon } from '@lucide/svelte';
+	import { page } from '$app/state';
+
+	const currentPath = page.url.pathname;
+
+	const routes = {
+		home: '/dashboard/home',
+		map: '/dashboard/maps',
+		settings: '/dashboard/settings'
+	};
+
+	const isActive = (/** @type {string} */ path) => currentPath === path;
+</script>
+<style>
+	.icon-base {
+		display: flex;
+		justify-content: center;
+        align-items: center;
+		width: 95%;
+		padding: 10px 0px;
+		cursor: pointer;
+        border-top-left-radius: 40px;
+        border-bottom-left-radius: 40px;
+	}
+	.icon-active {
+		background-color: rgb(236, 244, 255);
+        border-left: 1px solid rgb(185, 214, 255);
+        border-top: 1px solid rgb(185, 214, 255);
+        border-bottom: 1px solid rgb(185, 214, 255);
+	}
+    h2{
+        display: flex;
+        width: 50%;
+        margin-left: 10px;
+    }
+</style>
+<div class="swiper-slide menu border-r-1 border-gray-300">
+    <div class="w-full h-[70px]"></div>
+    <div class=" flex flex-col gap-3 pt-5 items-end">
+        <!--Dont change pt-5-->
+        <div class="icon-base {isActive(routes.home) ? 'icon-active' : ''}">
+            <Home size={50} absoluteStrokeWidth color="rgb(0, 43, 103)" />
+            <h2>Home</h2>
+        </div>
+        <div class="icon-base {isActive(routes.map) ? 'icon-active' : ''}">
+            <MapIcon size={50} absoluteStrokeWidth color="rgb(0, 43, 103)"  />
+            <h2>Maps</h2>
+        </div>
+        <div class="icon-base {isActive(routes.settings) ? 'icon-active' : ''}">
+            <Settings size={50} absoluteStrokeWidth color="rgb(0, 43, 103)"  />
+            <h2>Settings</h2>
+        </div>
+    </div>
+</div>
