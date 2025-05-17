0e5fb173ef98f52acb833ade217f1a58f87ca693
sierra-95 <michaelmachohi@gmail.com>
2025-05-17 21:34:11 +0300

    Turnstile cloudflare

--- front-end/src/routes/login/+page.svelte
+++ front-end/src/routes/login/+page.svelte
@@ -8,40 +8,47 @@
   const handleLogin = () => {
     alert("Logging in");
   };
+  let turnstileToken = "";
 </script>
   
-  <style>  
-    label {
-      display: block;
-      margin-bottom: 0.5rem;
-      font-weight: 500;
-      color: #333;
-    }
-    input {
-      padding: 0.75rem;
-      border: 1px solid #E0E0E0;
-      border-radius: 4px;
-      font-size: 1rem;
-    }
-    input:focus {
-        outline: none;
-        box-shadow: none;
-    }
-    .password-container {
-      position: relative;
-    }
-    .toggle-password {
-      position: absolute;
-      right: 0.75rem;
-      top: 50%;
-      transform: translateY(-50%);
-      background: none;
-      border: none;
-      color: #333;
-      cursor: pointer;
-      font-size: 0.875rem;
-    }
-  </style>
+<style>  
+  label {
+    display: block;
+    margin-bottom: 0.5rem;
+    font-weight: 500;
+    color: #333;
+  }
+  input {
+    padding: 0.75rem;
+    border: 1px solid #E0E0E0;
+    border-radius: 4px;
+    font-size: 1rem;
+  }
+  input:focus {
+      outline: none;
+      box-shadow: none;
+  }
+  .password-container {
+    position: relative;
+  }
+  .toggle-password {
+    position: absolute;
+    right: 0.75rem;
+    top: 50%;
+    transform: translateY(-50%);
+    background: none;
+    border: none;
+    color: #333;
+    cursor: pointer;
+    font-size: 0.875rem;
+  }
+</style>
+
+
+<svelte:head>
+  <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
+</svelte:head>
+
 <main class="h-screen">
   <div style="height: calc(100vh - 100px);" class="flex">
     <form class="container flex flex-col gap-4 max-w-[600px] my-auto mx-auto max-[600px]:mx-6">  
@@ -61,7 +68,7 @@
             id="password" 
             bind:value={password} 
           />
-          <button class="toggle-password" on:click={() => showPassword = !showPassword}>
+          <button class="toggle-password" on:click|preventDefault={() => showPassword = !showPassword}>
             {#if showPassword}
               <Eye/>
             {:else}  
@@ -70,6 +77,14 @@
           </button>
         </div>
       </div>  
+
+      <div
+        class="cf-turnstile"
+        data-sitekey="0x4AAAAAABdtnf8oh8_LBi9N"
+        data-callback="onVerify"
+        data-theme="light"
+        data-size='flexible'
+      ></div>
       <Button 
         color="primary" 
         onClick={handleLogin} 
