1cc8cfde38af8ae9f4dc3483ae5dc249bd1521df
sierra-95 <michaelmachohi@gmail.com>
2025-05-17 13:26:57 +0300

    CLose menu after navigation

--- front-end/src/lib/components/logo.svelte
+++
@@ -1,3 +0,0 @@
-<header class="h-[70px] border-b-1 border-gray-200 ">
-    <img src="/dekut.png" alt="Logo" class="h-[60px]"/>
-</header>
--- front-end/src/lib/index.js
+++ front-end/src/lib/index.js
@@ -1,5 +1,4 @@
 export { default as Button } from './components/button.svelte';
-export { default as Logo } from './components/logo.svelte';
 export { default as Header } from './components/Layout/header.svelte';
 export { default as SideBar } from './components/Layout/side-bar.svelte';
 export { default as SideMenu } from './components/Layout/side-menu.svelte';
