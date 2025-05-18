dae8577204dede3de44e5c6aac3992a71d0a2e9f
sierra-95 <michaelmachohi@gmail.com>
2025-05-18 19:12:43 +0300

    remote SQLIte

--- setup/wrangler.sh
+++ setup/wrangler.sh
@@ -9,4 +9,7 @@ npx wrangler d1 execute agv-db --remote --file=./schema.sql #remote
 npx wrangler d1 execute agv-db --command="SELECT * FROM users;" #local
 npx wrangler d1 execute agv-db --remote --command="SELECT * FROM users;" #remote
 
+curl -X POST https://backend.michaelmachohi.workers.dev/login \
+  -H "Content-Type: application/json" \
+  -d '{"email":"2025agvrobotics.team@gmail.com", "password":"@team2025"}'
 
