157f75a7a75f56d5349ecc84c45f3105c6b9aed2
sierra-95 <michaelmachohi@gmail.com>
2025-05-19 15:50:40 +0300

    comments

--- front-end/src/routes/login/+page.svelte
+++ front-end/src/routes/login/+page.svelte
@@ -1,8 +1,38 @@
+<!--
+use:enhance is a Svelte action that allows you to enhance forms with AJAX-like behavior.
+It lets you run logic before and after the form submission, and it can be used to handle the response from the server.
+############Important############
+use enhance helps sveltkit to intercept form submission hence no page reload hence no need to use preventDefault
+form submission is then handled by the server(+page.server.js) and the response is returned to the client.
+
+After return,enhance calls eg handlesubmit(use:enhance={handleSubmit}) callback update
+update() cause the page to rerun with the updated form prop(export let form)
+-->
+
+<!--
+export let form;
+SvelteKit automatically injects it into your page if you submit a form 
+enhanced with use:enhance or just a normal HTML post to your page.
+As a result you maye access:
+- form.error: The error message returned from the server.
+- form.data: The data returned from the server.
+- form.status: The status code returned from the server.
+- form.method: The method used to submit the form.
+- form.success: A boolean indicating if the form submission was successful.
+-->
+
+<!--
+Actions:
+when a form submits, +page.server.js will be called. 
+it will look for a matching actions hence execute.
+
+export let form comes from the return of the action ################Important#########
+-->
 <script>
 	import { enhance } from '$app/forms';
   import {Button} from '$lib'
   import { Eye, EyeOff } from '@lucide/svelte';
-   import { page } from '$app/stores';
+  import { page } from '$app/stores';
   
   let email = '';
   let password = '';
@@ -15,9 +45,12 @@
     // @ts-ignore
     return async ({ update, result }) => {
       isLoading = false;
+      //This is the actual return value from your actions.default()
+      //if (!result.success) showToast(result.error)
       await update(result);
     };
   }
+  
 </script>
   
 <style>  
@@ -34,7 +67,6 @@
     <form
       method="POST" 
       use:enhance={handleEnhance}
-      on:submit={() => isLoading = true}
       class="container flex flex-col gap-4 max-w-[600px] my-auto mx-auto max-[600px]:mx-6"
     >  
       <h2 class="text-center text-3xl mb-4">Dekut 2025 AGV Robotics</h2>
