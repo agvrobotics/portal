b484b21a39c17cf97991fc629b261a6a54c1770a
sierra-95 <michaelmachohi@gmail.com>
2025-05-20 15:28:21 +0300

    turnstile

--- front-end/src/routes/dashboard/+layout.svelte
+++ front-end/src/routes/dashboard/+layout.svelte
@@ -58,18 +58,6 @@
   });
 </script>
 
-
-<svelte:head>
-  <link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
-  />
-  <script
-    src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"
-    defer
-  ></script>
-</svelte:head>
-
 <style>
   @import './layout.css';
 </style>
