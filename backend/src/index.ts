5b7c76a5d0d469810a8987804e63027ca18687f7
sierra-95 <michaelmachohi@gmail.com>
2025-05-22 00:08:49 +0300

    trouble

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,28 +1,12 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-import { testHandler } from './routes/test';
+
 import { loginHandler } from './routes/login';
 import {verifyHandler} from './routes/jwtverify';
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const url = new URL(request.url);
 		switch (url.pathname) {
-			case '/message':
-				return new Response('Welcome to agvrobotics!');
 			case '/random':
 				return new Response(crypto.randomUUID());
-			case '/test':
-				return testHandler(request);
 			case '/login':
 				return loginHandler(request, env);
 			case '/jwtverify':
--- backend/src/routes/send-message.ts
+++ backend/src/routes/send-message.ts
@@ -2,8 +2,8 @@ interface Env {
   AGV_WS: DurableObjectNamespace;
 }
 export async function sendMessage(request: Request, env: Env) {
-  const { message } = await request.json();
-  const agvId = "test-agv"; // Hardcode for today
+  const { message } = (await request.json()) as { message: string };
+  const agvId = "test-agv";
   const doId = env.AGV_WS.idFromName(agvId);
   const stub = env.AGV_WS.get(doId);
   return stub.fetch("http://do/internal", {
