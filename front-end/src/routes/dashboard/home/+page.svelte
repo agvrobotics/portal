793797b4535c5bbaa98acca519df757ffeff7ea9
sierra-95 <michaelmachohi@gmail.com>
2025-05-22 19:24:11 +0300

    frontend

--- front-end/src/routes/dashboard/home/+page.svelte
+++ front-end/src/routes/dashboard/home/+page.svelte
@@ -1,48 +1,58 @@
 <script>
-  import { Power,Zap, Moon } from '@lucide/svelte';
-  import {Button} from '$lib'
+    import { Power,Zap, Moon, Bot, ArrowUpRight } from '@lucide/svelte';
+    import {Button} from '$lib'
+    
+    let agvs = [
+        {
+        name: 'Alpha',
+        id: ' f59a2af3-1bd4-4480-99e2-d843c2a8a948',
+        status: 'hibernation',
+        },
+    ];
 
-  let agvs = [
-    {
-      name: 'Alpha',
-      id: ' f59a2af3-1bd4-4480-99e2-d843c2a8a948',
-      status: 'hibernation',
-    },
-  ];
+    /**
+	 * @param {{ status: string; }} agv
+	 */
+    function startAGV(agv) {
+        agv.status = 'online';
+        agvs = [...agvs];
+    }
+    /**
+	 * @param {{ status: string; }} agv
+	 */
+    function sleepAGV(agv) {
+        agv.status = 'sleep';
+        agvs = [...agvs];
+    }
+    /**
+	 * @param {{ status: string; }} agv
+	 */
+    function shutdownAGV(agv) {
+        agv.status = 'hibernation';
+        agvs = [...agvs];
+    }
 
-  // @ts-ignore
-  function startAGV(agv) {
-    agv.status = 'online';
-  }
-  // @ts-ignore
-  function sleepAGV(agv) {
-    agv.status = 'sleep';
-  }
-  // @ts-ignore
-  function shutdownAGV(agv) {
-    agv.status = 'hibernation';
-  }
     const actions = [
         {
         name: 'Start',
         icon: Zap,
         status: 'online',
         class: 'start',
-        handler: (/** @type {{ status: string; }} */ agv) => agv.status = 'online',
+        handler: startAGV,
         },
         {
         name: 'Sleep',
         icon: Moon,
         status: 'sleep',
         class: 'sleep',
-        handler: (/** @type {{ status: string; }} */ agv) => agv.status = 'sleep',
+        handler: sleepAGV,
         },
         {
         name: 'Shutdown',
         icon: Power,
         status: 'hibernation',
         class: 'shutdown',
-        handler: (/** @type {{ status: string; }} */ agv) => agv.status = 'hibernation',
+        handler: shutdownAGV,
         },
     ];
 </script>
@@ -53,27 +63,33 @@
 
 <div class="grid">
   {#each agvs as agv}
-    <div class="card flex flex-col gap-2">
-      <h2 class="text-left text-[1.2rem]">
-        <span class="font-bold">{agv.name}</span>
-        <span class="status-indicator status-{agv.status}">Status: {agv.status}</span>
-      </h2>
-      <div class="text-[#666] text-[1rem] text-left">{agv.id}</div>
-      <hr class="text-[#D1D5DB]" />
+    <div class="card flex flex-col gap-2 relative">
+        <h2 class="text-left text-[1.2rem] flex ">
+            <span class="flex">
+                <Bot color="rgb(0, 43, 103)"/>
+                <span class="font-bold ml-2 mr-2">{agv.name}</span>
+            </span>
+            <span class="status-indicator status-{agv.status}">Status: {agv.status}</span>
+        </h2>
+        <h2 class="absolute right-5 text-[0.9rem] text-[#056DFF]  leading-4 cursor-pointer flex">
+            <span class="border-b-1 border-[#056DFF]">View details</span><ArrowUpRight size={20}/>
+        </h2>
+        <div class="text-[#666] text-[1rem] text-left">{agv.id}</div>
+        <hr class="text-[#D1D5DB]" />
 
-      <div class="flex gap-2">
-        {#each actions as action}
-          {#if agv.status !== action.status}
-            <Button 
-              onClick={() => action.handler(agv)}
-              externalClass={`${action.class} btn`}
-            >
-              <svelte:component this={action.icon} size="16" />
-              {action.name}
-            </Button>
-          {/if}
-        {/each}
-      </div>
+        <div class="flex gap-2">
+            {#each actions as action}
+            {#if agv.status !== action.status}
+                <Button 
+                onClick={() => action.handler(agv)}
+                externalClass={`${action.class} btn`}
+                >
+                <svelte:component this={action.icon} size="16" />
+                {action.name}
+                </Button>
+            {/if}
+            {/each}
+        </div>
     </div>
   {/each}
 </div>
