9b4520bcdaf92600782cc43266a751acc9d005d4
sierra-95 <michaelmachohi@gmail.com>
2025-10-25 16:19:16 +0300

    switching to type script

--- front-end/src/routes/login/+page.svelte
+++ front-end/src/routes/login/+page.svelte
@@ -1,33 +1,3 @@
-<!--
-use:enhance is a Svelte action that allows you to enhance forms with AJAX-like behavior.
-It lets you run logic before and after the form submission, and it can be used to handle the response from the server.
-############Important############
-use enhance helps sveltkit to intercept form submission hence no page reload hence no need to use preventDefault
-form submission is then handled by the server(+page.server.js) and the response is returned to the client.
-
-After return,enhance calls eg handlesubmit(use:enhance={handleSubmit}) callback update
-update() cause the page to rerun with the updated form prop(export let form)
--->
-
-<!--
-export let form;
-SvelteKit automatically injects it into your page if you submit a form 
-enhanced with use:enhance or just a normal HTML post to your page.
-As a result you maye access:
-- form.error: The error message returned from the server.
-- form.data: The data returned from the server.
-- form.status: The status code returned from the server.
-- form.method: The method used to submit the form.
-- form.success: A boolean indicating if the form submission was successful.
--->
-
-<!--
-Actions:
-when a form submits, +page.server.js will be called. 
-it will look for a matching actions hence execute.
-
-export let form comes from the return of the action ################Important#########
--->
 <script>
 	import { enhance } from '$app/forms';
   import {Button} from '@sierra-95/svelte-ui';
@@ -38,26 +8,15 @@ export let form comes from the return of the action ################Important###
   let password = '';
   let showPassword = false;
   let isLoading = false;
-  let showCaptcha = true;
 
   function handleEnhance() {
     isLoading = true;
     // @ts-ignore
     return async ({ update, result }) => {
       isLoading = false;
-      //This is the actual return value from your actions.default()
-      //if (!result.success) showToast(result.error)
       await update(result);
     };
-  }
-  $: if ($page.form?.error) {
-		// Hide and re-show the CAPTCHA to allow multiple submissions
-		showCaptcha = false;
-		setTimeout(() => {
-			showCaptcha = true;
-		}, 10);
-	}
-  
+  } 
   
 </script>
   
@@ -66,9 +25,9 @@ export let form comes from the return of the action ################Important###
 </style>
 
 
-<svelte:head>
+<!-- <svelte:head>
   <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
-</svelte:head>
+</svelte:head> -->
 
 <main class="h-screen">
   <div style="height: calc(100vh - 100px);" class="flex">
@@ -82,13 +41,10 @@ export let form comes from the return of the action ################Important###
       {#if $page.form?.error}
         <div class="error text-red-500">{$page.form.error}</div>
       {/if}
-      
-      <!--Email-->
       <div class="w-full">
         <label for="email">Email</label>
         <input class="w-full" type="email" id="email" name="email" bind:value={email}/>
       </div>
-      <!--Password-->
       <div class="w-full">
         <label for="password">Password</label>
         <div class="password-container">
@@ -108,8 +64,7 @@ export let form comes from the return of the action ################Important###
           </button>
         </div>
       </div>  
-      <!--Turnstile-->
-      {#if showCaptcha}
+      <!-- {#if showCaptcha}
         <div
           class="cf-turnstile"
           data-sitekey="0x4AAAAAABdtnf8oh8_LBi9N"
@@ -117,12 +72,10 @@ export let form comes from the return of the action ################Important###
           data-theme="light"
           data-size='flexible'
         ></div>
-      {/if}
+      {/if} -->
       <Button 
-        color="primary" 
-        submit={true}
+        type="submit"
         isLoading={isLoading}
-        externalClass="mt-4 w-full"
       >Log In</Button>  
     </form>
   </div>
