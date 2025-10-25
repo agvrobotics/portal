101bcb23af5018974f820d0483bae080decd5911
sierra-95 <michaelmachohi@gmail.com>
2025-10-25 19:20:03 +0300

    menu upgrade

--- front-end/src/lib/components/Layout/header.svelte
+++ front-end/src/lib/components/Layout/header.svelte
@@ -20,7 +20,7 @@
   @import './header.css';
 </style>
 <header class="w-full flex justify-between h-[70px] border-b-1 border-gray-300">
-  <div class="flex items-center div2">
+  <div class="flex items-center">
     <div class="menu-button ml-2 cursor-pointer" role="none" on:click={toggleMenu}>
       <div class="bar"></div>
       <div class="bar"></div>
