126f262588b528fe3d97f52a958c0044d4aac327
sierra-95 <michaelmachohi@gmail.com>
2025-10-25 19:22:37 +0300

    menu upgrade

--- front-end/src/lib/index.js
+++ front-end/src/lib/index.js
@@ -1,8 +1,6 @@
 //Components
 export { default as Header } from './components/Layout/header.svelte';
 export {default as Menu } from './components/Layout/Menu/menu.svelte';
-
-export {default as Toast} from './components/toast.svelte'
 export {default as Modal} from './components/modal.svelte'
 //Stores
 export {user} from './stores/user'
