a44ec0e09081f78752f8d8672b82012cae59bbac
sierra-95 <michaelmachohi@gmail.com>
2025-10-26 16:59:45 +0300

    removed modal

--- front-end/src/lib/index.js
+++ front-end/src/lib/index.js
@@ -1,7 +1,5 @@
 //Components
 export { default as Header } from './components/Layout/header.svelte';
 export {default as Menu } from './components/Layout/Menu/menu.svelte';
-export {default as Modal} from './components/modal.svelte'
 //Stores
 export {user} from './stores/user'
-export {modalStore} from './stores/modal'
