4f9246f4a7dbd2e698c2398532dee616656f9eef
sierra-95 <michaelmachohi@gmail.com>
2025-10-30 22:36:16 +0300

    .env file

---
+++ front-end/src/lib/server/verifyjwt.ts
@@ -0,0 +1,14 @@
+import {api} from '$env/static/private'
+
+export async function verifyJWT(token : string) {
+  const res = await fetch(`${api}auth/jwtverify`, {
+    method: 'GET',
+    headers: {
+      Cookie: `token=${token}`
+    }
+  });
+
+  if (!res.ok) return null;
+
+  return await res.json(); 
+}
